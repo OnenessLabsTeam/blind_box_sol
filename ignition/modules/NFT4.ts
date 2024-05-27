61bfe18103629347965f730b8e635b1fc34f8821
yanxiaobo <yanxiaobo@id.link>
2024-05-27 11:08:08 +0800

    add nft4

---
+++ ignition/modules/NFT4.ts
@@ -0,0 +1,14 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import args from "./NFT4_args";
+
+const NFTModule = buildModule("NFTModule", (m) => {
+    const nft = m.contract("NFT", args);
+
+    return { nft };
+});
+
+export default NFTModule;
+
+//address: 0x1220b06984D58d9CEFf4f1e4b84b4100388AAB5b
+//verify
+//npx hardhat verify --network polygon --constructor-args ./ignition/modules/NFT3_args.ts  0x1220b06984D58d9CEFf4f1e4b84b4100388AAB5b
